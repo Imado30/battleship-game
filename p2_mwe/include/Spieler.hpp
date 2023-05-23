b1d8bf586e000d71d0d1a450d3a01bb6c98e3c3e
Christoph Mauel <s23cmaue@uni-bonn.de>
2023-05-23 12:24:12 +0200

    Spieler.hpp eingefügt

---
+++ p2_mwe/include/Spieler.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include <iostream>
+
+namespace SchiffeVersenken{
+
+    class Spieler{
+    public:
+        Spieler(std::string Name);
+
+        void Schießen(int Koord1, int Koord2);
+
+        bool hit_or_miss(int Koord1, int Koord2);
+
+        void markieren(int Koord1, int Koord2);
+
+    private:
+        std::string Name;
+
+        int ID;
+    };
+
+}
